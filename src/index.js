58f04e87d5d1212f36aee601106c278e2bfe03ca
Alexismarie20 <alexismhepler@gmail.com>
2025-05-25 22:43:17 -0400

    added user experience

--- src/index.js
+++ src/index.js
@@ -19,14 +19,7 @@ let prompt = `User instructions: Generate a short 4 lines motivational poem abou
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 let poemElement = document.querySelector("#poem");
 poemElement.classList.remove("hidden");
-
-
-
-
 poemElement.innerHTML = `<div class = "generating">⏳ Generating your poem about ${instructionsInput.value}...</div>`;
-
-console.error("Element not found!");
-
 
 axios.get(apiUrl).then(displayPoem);
 }
